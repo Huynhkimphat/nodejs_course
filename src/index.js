c1a08bbde9ad576afea187f4f2ad65ad4e8d408f
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-02-17 11:48:30 +0700

    Update Template Engine

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const handlebars = require('express-handlebars');
+const port = 3000;
+
+// HTTP Logger
+app.use(morgan('combined'));
+
+// Template Engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Render
+app.get('/homepage', (req, res) => {
+    res.render('home');
+});
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
