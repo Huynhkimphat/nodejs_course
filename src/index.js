30daac7799f51bc56e524222a6557698329b911d
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-02-18 17:33:32 +0700

    Update Controller and Routes

--- src/index.js
+++ src/index.js
@@ -5,25 +5,26 @@ const app = express();
 const handlebars = require('express-handlebars');
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
-    // HTTP Logger
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+// * HTTP Logger
 app.use(morgan('combined'));
 
-// Template Engine
+// * Template Engine
 app.engine('hbs', handlebars({
     extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// Render
-app.get('/homepage', (req, res) => {
-    res.render('home');
-});
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
+// * Route Init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
